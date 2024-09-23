cd887d4d2063181937d788bab155848f927845e3
dieuska <karel.dieussaert@geosolutions.be>
2024-09-23 09:55:42 +0200

    updated version numbr and CHANGES

--- autoupdateborders.py
+++ autoupdateborders.py
@@ -3,7 +3,7 @@
 """
 ***************************************************************************
 *   name: brdrQ - AutoUpdateBorders
-*   version: v0.9.7beta
+*   version: v0.9.7
 *   author: Karel Dieussaert
 *   Docs and Code- repo: https://github.com/OnroerendErfgoed/brdrQ/
 *   history:
