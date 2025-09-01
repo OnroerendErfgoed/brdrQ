074bd5a4a22f22902d4b6f2ffbf6ba62e5d2f876
dieuska <karel.dieussaert@geosolutions.be>
2025-09-01 16:47:37 +0200

    module import improvement

--- plugin/brdrq/brdrq_module_importer.py
+++ plugin/brdrq/brdrq_module_importer.py
@@ -48,13 +48,17 @@ def import_modules():
             raise ValueError("Version mismatch")
 
     except (ModuleNotFoundError, ValueError):
+
+        if "brdr" in sys.modules:
+            del sys.modules["brdr"]
+            print ("brdr removed from sys_module")
+
         subprocess.check_call(
             [python_exe, "-m", "pip", "install", "brdr==" + brdr_version]
         )
         # show_new_brdr_dialog()
         import brdr
-
-        print(f"version of brdr before reload: {brdr.__version__}")
+        #print(f"version of brdr before reload: {brdr.__version__}")
         importlib.reload(brdr)
         import brdr
 
