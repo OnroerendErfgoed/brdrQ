6d3cc29cec7402485c27976a820c18055c2251bf
dieuska <karel.dieussaert@geosolutions.be>
2025-10-14 18:11:47 +0200

    Update brdrq_module_importer.py

--- plugin/brdrq/brdrq_module_importer.py
+++ plugin/brdrq/brdrq_module_importer.py
@@ -28,22 +28,17 @@ def find_python():
 
     raise Exception("Python executable not found")
 
-# def pipinstall_by_subprocess2(python_exe,package):
-#     plugin_dir = os.path.dirname(__file__)
-#     libs_dir = os.path.join(plugin_dir, "libs")
-#
-#     # Voeg libs toe aan sys.path
-#     if libs_dir not in sys.path:
-#         sys.path.append(libs_dir)
-#
-#     subprocess.check_call(
-#                 [python_exe, "-m", "pip", "install", package,"--target",libs_dir]
-#             )
-
 def pipinstall_by_subprocess(python_exe,package):
     if sys.platform != "win32":
-        user_profile_name = iface.userProfileManager().userProfile().name()
-        target_dir="~/.local/share/QGIS/QGIS3/profiles/" + user_profile_name + "/python"
+        # user_profile_name = iface.userProfileManager().userProfile().name()
+        # target_dir = os.path.expanduser(
+        #     os.path.join(
+        #         "~/.local/share/QGIS/QGIS3/profiles", user_profile_name, "python"
+        #     )
+        # )
+        plugin_dir = os.path.dirname(__file__)
+        target_dir = os.path.join(plugin_dir, "libs")
+
         # Add target_dir to sys.path
         if target_dir not in sys.path:
             sys.path.append(target_dir)
